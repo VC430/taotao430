49ad476324f60b6995f67eca4d211afd91afecf9
song <songlqiuucstao13@gmail.com>
2021-05-24 17:05:07 +0800

    商品缓存

--- taotao-parent/taotao-search/taotao-search-interface/src/main/java/com/taotao/search/service/SearchService.java
+++
@@ -1,7 +0,0 @@
-package com.taotao.search.service;
-
-import com.taotao.common.pojo.SearchResult;
-
-public interface SearchService {
-    SearchResult search(String queryString, int pge, int rows) throws Exception;
-}
